c1d853af554db3d48fab787f065457a746003064
JuliaLuka <lukashova.yuliia26@gmail.com>
2023-07-30 21:29:21 +0200

    add

---
+++ index.js
@@ -0,0 +1,75 @@
+// // 1
+// const productName = "Droid";
+// const pricePerItem = 2000;
+
+// console.log(productName);
+// console.log(pricePerItem);
+
+//2
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// productName = "Repair droid";
+// console.log(productName);
+
+// pricePerItem = 3500;
+// console.log(pricePerItem);
+
+// 3
+// const topSpeed = 160;
+// console.log(topSpeed);
+// const distance = 617.54;
+// console.log(distance);
+// const login = "mango935";
+// console.log(login);
+// const isOnline = true;
+// console.log(isOnline);
+// const isAdmin = false;
+// console.log(isAdmin);
+
+// 4
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+
+// 5
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+// 6
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+// 7
+// function sayHi() {
+//     console.log("Hello, this is my first function!");
+// }
+// sayHi();
+
+// 8
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+// }
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// 9
+// function add(a, b, c) {
+//   return a + b + c;
+// }
+// add(2, 5, 8);
+
+// let result = add(15, 27, 10);
+// console.log(result);
+// result = add(10, 20, 30);
+// console.log(result);
+// result = add(5, 10, 15);
+// console.log(result);

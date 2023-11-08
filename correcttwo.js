db1d6d7dfa004216bf068cf78b4a5c94c601e503
JuliaLuka <lukashova.yuliia26@gmail.com>
2023-11-08 12:44:50 +0100

    Update correcttwo.js

--- correcttwo.js
+++ correcttwo.js
@@ -1,16 +1,8 @@
-// // 22
-function createArrayOfNumbers(min, max) {
-  const numbers = [];
-
-  for (i = min; i <= max; i += 1) {
-    numbers = min.push(max);
-  }
-  return numbers;
+function splitMessage(message, delimiter) {
+  let words;
+  // Change code below this line
+  words = message.split(delimiter);
+  // Change code above this line
+  return words;
 }
-console.log(createArrayOfNumbers(29, 34));
-// Просим клиента подтвердить бронь на отель
-// и сохраняем в переменную результат работы confirm
-// Спрашиваем имя отеля в котором хотел бы остановится клиент
-// и сохраняем в переменную результат вызова prompt.
-console.log(0.17 + 0.24); // 0.41000000000000003
-console.log((0.17 + 0.24).toFixed(2)); // 0.41
+splitMessage("Mango hurries to the train", " ");
